ccca742d5bb25b46fdd69a06083cd378961ea00c
megha <megha.patel2@sheridancollege.ca>
2023-01-19 13:39:47 -0500

    first commit

---
+++ src/stringreverse/StringReverse.java
@@ -0,0 +1,34 @@
+
+package stringreverse;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class StringReverse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String name=sc.nextLine();
+        
+        char[] arr=new char[name.length()];
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=name.charAt(i);
+        }
+        
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]);
+        }
+        System.out.println("");
+    }
+
+}
